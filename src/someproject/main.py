11ead7943e60d471240ec0260f0a338455093c2c
eborchardt <56270998+eborchardt@users.noreply.github.com>
2024-02-16 16:34:02 -0600

    Update main.py

--- src/someproject/main.py
+++ src/someproject/main.py
@@ -1,27 +1,8 @@
-import argparse
-import logging
-
-
-def print_message(use_log, message):
-    if use_log:
-        logging.getLogger(__name__).info(message)
-    else:
-        print(message)
-
+import requests
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--uselog", action="store_true")
-    args = parser.parse_args()
+    exampleVariable = "asdfasdf"
+    print(f"##teamcity[setParameter name='someParameter' value='{exampleVariable}']")
 
-    if args.uselog:
-        logging.basicConfig(level=logging.INFO)
-
-    print_message(args.uselog, "##teamcity[inspectionType id='id' name='name' category='Medium Severity' description='description']")
-    print_message(args.uselog, "##teamcity[inspection typeId='id' message='message 1' file='filename.c' line='1' SEVERITY='Medium Severity']")
-    print_message(args.uselog, "##teamcity[inspection typeId='id' message='message 2' file='filename.c' line='2' SEVERITY='Medium Severity']")
-    print_message(args.uselog, "##teamcity[inspection typeId='id' message='message 3' file='filename.c' line='3' SEVERITY='Medium Severity']")
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
