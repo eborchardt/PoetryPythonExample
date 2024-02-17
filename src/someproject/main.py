99f479f0bc76ba68b6b5c2ac7b8a3877c2998db8
eborchardt <56270998+eborchardt@users.noreply.github.com>
2024-02-16 19:05:19 -0600

    Update main.py

--- src/someproject/main.py
+++ src/someproject/main.py
@@ -1,8 +1,10 @@
 import requests
 
 def main():
-    exampleVariable = "asdfasdf"
-    print(f"##teamcity[setParameter name='someParameter' value='{exampleVariable}']")
+    print("##teamcity[inspectionType id='id' name='name' category='Medium Severity' description='description']")
+    print("##teamcity[inspection typeId='id' message='message 1' file='filename.c' line='1' SEVERITY='Medium Severity']")
+    print("##teamcity[inspection typeId='id' message='message 2' file='filename.c' line='2' SEVERITY='Medium Severity']")
+    print("##teamcity[inspection typeId='id' message='message 3' file='filename.c' line='3' SEVERITY='Medium Severity']")
 
 if __name__ == "__main__":
     main()
