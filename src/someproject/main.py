92448e385c4ae84fbf3bae7961f0bfee890dcd61
eborchardt <56270998+eborchardt@users.noreply.github.com>
2024-02-16 19:27:52 -0600

    Update main.py

--- src/someproject/main.py
+++ src/someproject/main.py
@@ -17,7 +17,11 @@ def main():
     if args.uselog:
         logging.basicConfig(level=logging.INFO)
 
-    print_message(args.uselog, "##teamcity[message text='Exception text' errorDetails='stack trace' status='ERROR']")
+    print_message(args.uselog, "##teamcity[inspectionType id='id' name='name' category='Medium Severity' description='description']")
+    print_message(args.uselog, "##teamcity[inspection typeId='id' message='message 1' file='filename.c' line='1' SEVERITY='Medium Severity']")
+    print_message(args.uselog, "##teamcity[inspection typeId='id' message='message 2' file='filename.c' line='2' SEVERITY='Medium Severity']")
+    print_message(args.uselog, "##teamcity[inspection typeId='id' message='message 3' file='filename.c' line='3' SEVERITY='Medium Severity']")
+
 
 if __name__ == '__main__':
     main()
