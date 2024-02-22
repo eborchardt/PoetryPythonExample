956da38f8a53caac7d3558e1cd81584517cbc88f
eborchardt <56270998+eborchardt@users.noreply.github.com>
2024-02-22 15:32:44 -0600

    Update main.py

--- src/someproject/main.py
+++ src/someproject/main.py
@@ -2,12 +2,12 @@ from teamcity.messages import TeamcityServiceMessages
 
 def send_inspection_messages():
     # Send inspection type message
-    inspection_type_message = "##teamcity[inspectionType id='id' name='name' category='Medium Severity' description='description']"
+    inspection_type_message = "inspectionType id='id' name='name' category='Medium Severity' description='description'"
     TeamcityServiceMessages().message(inspection_type_message)
 
     # Send inspection messages
     for i in range(1, 4):
-        inspection_message = f"##teamcity[inspection typeId='id' message='message {i}' file='filename.c' line='{i}' SEVERITY='Medium Severity']"
+        inspection_message = f"inspection typeId='id' message='message {i}' file='filename.c' line='{i}' SEVERITY='Medium Severity'"
         TeamcityServiceMessages().message(inspection_message)
 
 if __name__ == "__main__":
